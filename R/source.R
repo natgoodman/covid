ca2dbd59cd4c2b35deda2dfc724ea57dbddacb99
natgoodman <natg@shore.net>
2020-10-25 11:08:06 -0700

    Move functions to 'util/R/source.R' Modify utility source files to point ot 'util' directory tree

--- R/source.R
+++ R/source.R
@@ -20,37 +20,40 @@
 #################################################################################
 ## SOURCE=c('R/util.R','R/datman.R','R/doc.R','R/init.R','R/stats.R');
 ## SOURCE=c('R/util.R','R/init.R','R/doc_hiddn.R');
-source('R/util.R');                     # source util first to get 'cq'
+source('../util/R/source.R');          # source util first to get 'cq'
 SOURCE=cq(cvdat,dat,data_cvdat,doc,edit,extra,fit,import,import_doh,init,label,pal,pal_yarrr,
-          meta,plot_cvdat,plotm,series,transform,util,util_dat,util_file,util_plot,workflow);
-SOURCE=sapply(SOURCE,function(src) paste0('R/',src,'.R'));
+          meta,plot_cvdat,plotm,series,transform,workflow);
+UTIL=cq(util,dat,file,plot);
+SOURCE=c(sapply(SOURCE,function(src) paste0('R/',src,'.R')),
+         sapply(UTIL,function(src) paste0('../util/R/',src,'.R')));
 
-## ---- source the files ----
-## source default files. assume README doc until init runs
-source_files=function(files=SOURCE) {
-  sapply(files,source);
-  invisible();
-}
-## ## source doc-specific files
-## source_doc=function(doc=param(doc)) {
-##   docr=paste(sep='.',doc,'R');
-##   source_ifexists(paste(sep='_','R/dat',docr));
-##   source_ifexists(paste(sep='_','R/doc',docr));
-##   source_ifexists(paste(sep='_','R/docfun',docr));
-##   source_ifexists(paste(sep='_','R/plot',docr));
-##   source_ifexists(paste(sep='_','R/stats',docr));
+## these functions now defined in util/R/source.R
+## ## ---- source the files ----
+## ## source default files. assume README doc until init runs
+## source_files=function(files=SOURCE) {
+##   sapply(files,source);
+##   invisible();
 ## }
-## source optional files
-source_ifexists=function(file) if (file.exists(file)) source(file);
+## ## ## source doc-specific files
+## ## source_doc=function(doc=param(doc)) {
+## ##   docr=paste(sep='.',doc,'R');
+## ##   source_ifexists(paste(sep='_','R/dat',docr));
+## ##   source_ifexists(paste(sep='_','R/doc',docr));
+## ##   source_ifexists(paste(sep='_','R/docfun',docr));
+## ##   source_ifexists(paste(sep='_','R/plot',docr));
+## ##   source_ifexists(paste(sep='_','R/stats',docr));
+## ## }
+## ## source optional files
+## source_ifexists=function(file) if (file.exists(file)) source(file);
                          
-## source all files
-## NG 19-09-10: can't call param(doc) in empty workspace - param.env doens't exist
-## source_all=function(files=SOURCE,doc=param(doc)) {
-source_all=function(files=SOURCE) {
-  source_files(files);
-  ## source dat_XXX, doc_XXX files so top level functions defined
-  ## NOTE: these top level functions call init which re-sources doc-specific files
-  ## source_files(list.files('R',pattern='^(doc_|dat_).*.R',full.names=T));
-  ## if (exists('param.env')) source_doc();
-}
+## ## source all files
+## ## NG 19-09-10: can't call param(doc) in empty workspace - param.env doens't exist
+## ## source_all=function(files=SOURCE,doc=param(doc)) {
+## source_all=function(files=SOURCE) {
+##   source_files(files);
+##   ## source dat_XXX, doc_XXX files so top level functions defined
+##   ## NOTE: these top level functions call init which re-sources doc-specific files
+##   ## source_files(list.files('R',pattern='^(doc_|dat_).*.R',full.names=T));
+##   ## if (exists('param.env')) source_doc();
+## }
 source_all();
