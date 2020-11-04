30310f6a8c4f59036275829ebf096f12188bca49
natgoodman <natg@shore.net>
2020-11-04 08:03:55 -0800

    Major rewrite to support new scheme for getting geoids and handling non-Washington places Now get geoids from Census Bureau Gazetteer file instead of 'acs5yr' file - old scheme was   real crock... Store geoids of interest in 'geo' file Use 'stateid' file to map state names to two letter IDs (eg, 'Iowa' to 'IA') Rewrite 'places_all', 'places_wa', 'places_other' functions to use 'geo' file Rename 'popbyage' to 'pop' for consistency with rest of code Add code for 'geo' and 'stateid'

--- R/meta.R
+++ R/meta.R
@@ -14,102 +14,70 @@
 #################################################################################
 library("rjson");
 ## ---- Import population metadata ----
-## creates meta/popbyage .Rdata, .txt
-## CAUTION: download sometimes fails mid-stream. no idea why... 
-download_popbyage=function(places=NULL,acs5yr=param(acs5yr)) {
-  geos=read.csv(acs5yr,stringsAsFactors=F);
-  ## skip first row (USA) and fix 2nd row (state)
-  if (geos$name[1]!='United States')
-    stop("First name in acs file should be 'United States', not ",geos$name[1]);
-  if (geos$name[2]!='Washington')
-    stop("Second name in acs file should be 'Washington', not ",geos$name[2]);
-  geos=geos[-1,1:2];
-  colnames(geos)=cq(geoid,place);
-  geos[1,'place']='state';
-  ## clean up county names. 
-  geos$place=sub(' County, WA','',geos$place);
-  if (!is.null(places)) geos=subset(geos,subset=place%in%places);
-  ## escape spaces in places like Walla Walla. do it after filtering places
-  geos$place=gsub(' ','\\ ',geos$place,fixed=TRUE);
-  ## construct wget commands, then do it!
-  wgets=with(geos,{
-    url=paste0('https://api.censusreporter.org/1.0/data/show/latest?table_ids=B01001&geo_ids=',
-               geoid);
-    qurl=paste0('"',url,'"');
-    ## construct output filename with 'paste' because 'filename' elides name vector
-    outfile=paste0(param(acsplacedir),'/',geos$place,'.json')
-    wget=paste('wget',qurl,'-O',outfile);
-  })
-  ## do it!
-  sapply(wgets,function(wget) {
-    if (param(verbose)) print(wget);
-    system(wget);
-  })
+## creates meta/pop .Rdata, .txt
+download_pop=function(geo=param(geo),placedir=param(placedir)) {
+  if (param(verbose)) print(paste('>>> downloading pop to',placedir));
+  if (is.null(geo)) geo=load_geo();
+  withrows(geo,row,download_place(geoid,place,placedir));
+  nrow(geo);
 }
-## geoids are from CensusReporter website. place used for file name
-download_annarbor=function(geoid='05000US26161',place='Washtenaw_MI') download_place(geoid,place)
-download_omaha=function(geoid='05000US31055',place='Douglas_NE') download_place(geoid,place)
 ## download one county from one state
-download_place=function(geoid,place) {
+download_place=function(geoid,place,placedir=param(placedir)) {
   url=paste0('https://api.censusreporter.org/1.0/data/show/latest?table_ids=B01001&geo_ids=',
              geoid);
   qurl=paste0('"',url,'"');
+  qplace=paste0('"',place,'"');
   ## construct output filename with 'paste' because 'filename' elides name vector
-  outfile=paste0(param(acsplacedir),'/',place,'.json')
+  outfile=paste0(placedir,'/',qplace,'.json')
   wget=paste('wget',qurl,'-O',outfile);
   if (param(verbose)) print(wget);
   system(wget);
 } 
-import_popbyage=
-  function(places=NULL,base=param(popbyage),
-           ages.all=c("all","0_19","20_39","40_59","60_79","80_")) {
-    dir=param(acsplacedir);
-    metafile=param(acsmeta);
-    places.all=sub('.json','',list.files(dir,pattern='.json',full.names=FALSE));
-    if (is.null(places)) places=places.all
-    else {
-      bad=places%notin%places.all;
-      if (any(bad)) stop('Invalid places(s): ',paste(collapse=', ',places[bad]));
-    }
-    metanames=meta_names(metafile);
-    pop=do.call(cbind,lapply(places,function(place) import_pop1(dir,place,metanames,ages.all)));
-    colnames(pop)=places;
-    save_popbyage(pop,base=base);
+import_pop=function(places=NULL,base=param(pop.file),placedir=param(placedir),
+                    metafile=param(acsmeta),ages.all=param(ages.all)) {
+  places.all=sub('.json','',list.files(placedir,pattern='.json',full.names=FALSE));
+  if (is.null(places)) places=places.all
+  else {
+    bad=places%notin%places.all;
+    if (any(bad)) stop('Invalid places(s): ',paste(collapse=', ',places[bad]));
   }
-import_pop1=
-  function(dir,place,metanames=NULL,
-           ages.all=c("all","0_19","20_39","40_59","60_79","80_")) {
-    if (is.null(metanames)) metanames=meta_names(param(acsmeta));
-    file=filename(dir,place,suffix='.json');
-    if (param(verbose)) print(paste('>>> importing',file));
-    json=fromJSON(file=file);
-    data=unlist(json$data[[1]]$B01001$estimate);
-    ## import variables from metanames
-    list2env(metanames,environment());
-    names(data)=names;
-    data.all=data[i.all]
-    data.male=data[i.male]
-    data.female=data[i.female]
-    data.both=data.male[-1]+data.female[-1]
-    ## for sanity, make sure totals match individual values
-    if (data.all!=(data.male['male']+data.female['female']))
-      stop("Bad news: data.all != male + female values");
-    if (data.male[1]!=sum(data.male[-1]))
-      stop("Bad news: male total != sum of male values");
-    if (data.female[1]!=sum(data.female[-1]))
-      stop("Bad news: female total != sum of female values");
-    if (data.all!=sum(data.both))
-      stop("Bad news: data.all != sum of combined male + female values");
-    ## combine columns into age groups of interest
-    names=names(data.both);
-    ages=strsplit(names,'_');
-    ages=apply(do.call(rbind,ages),2,as.integer);
-    cats=cut(ages[,1],c(0,20,40,60,80,100),right=F,labels=ages.all[-1]);
-    groups=split(names,cats);
-    pop=c(data.all,sapply(groups,function(j) sum(data.both[j])));
-    pop=data.frame(pop);
-    pop;
-  }
+  metanames=meta_names(metafile);
+  pop=do.call(cbind,lapply(places,function(place) import_pop1(placedir,place,metanames,ages.all)));
+  colnames(pop)=places;
+  save_pop(pop,base=base);
+}
+import_pop1=function(placedir,place,metanames=NULL,ages.all=param(ages.all)) {
+  if (is.null(metanames)) metanames=meta_names(param(acsmeta));
+  file=filename(placedir,place,suffix='.json');
+  if (param(verbose)) print(paste('>>> importing pop',file));
+  json=fromJSON(file=file);
+  data=unlist(json$data[[1]]$B01001$estimate);
+  ## import variables from metanames
+  list2env(metanames,environment());
+  names(data)=names;
+  data.all=data[i.all]
+  data.male=data[i.male]
+  data.female=data[i.female]
+  data.both=data.male[-1]+data.female[-1]
+  ## for sanity, make sure totals match individual values
+  if (data.all!=(data.male['male']+data.female['female']))
+    stop("Bad news: data.all != male + female values");
+  if (data.male[1]!=sum(data.male[-1]))
+    stop("Bad news: male total != sum of male values");
+  if (data.female[1]!=sum(data.female[-1]))
+    stop("Bad news: female total != sum of female values");
+  if (data.all!=sum(data.both))
+    stop("Bad news: data.all != sum of combined male + female values");
+  ## combine columns into age groups of interest
+  names=names(data.both);
+  ages=strsplit(names,'_');
+  ages=apply(do.call(rbind,ages),2,as.integer);
+  cats=cut(ages[,1],c(0,20,40,60,80,100),right=F,labels=ages.all[-1]);
+  groups=split(names,cats);
+  pop=c(data.all,sapply(groups,function(j) sum(data.both[j])));
+  pop=data.frame(pop);
+  pop;
+}
 ## process censusreport metadata file
 meta_names=function(metafile=param(acsmeta)) {
   json=fromJSON(file = metafile);
@@ -125,50 +93,40 @@ meta_names=function(metafile=param(acsmeta)) {
   list(names=names,i.all=which('all'==names),
        i.male=i.male1:(i.female1-1),i.female=i.female1:(length(names)));
 }
+## ---- Import geo & stateid metadata files
+## geo - get entries for WA and other, reformat, store in meta directory
+import_geo=function(infile=param(geo.infile),base=param(geo.file)) {
+  if (param(verbose)) print(paste('>>> importing geo',infile));
+  geo=read.delim(infile,stringsAsFactors=FALSE);
+  geo=geo[,cq(USPS,GEOID,NAME)];            # select columms we want
+  colnames(geo)=cq(state,geoid,county);     # change colnames per our convention
+  geo$geoid=paste0('05000US',sprintf("%05i",geo$geoid));
+                                            # CensusReporter county geoids are '05000US'+5 digits 
+  geo$county=sub(' County','',geo$county);  # clean up county names
+  ## get WA entries
+  geo.wa=subset(geo,subset=state=='WA');
+  colnames(geo.wa)[3]='place';
+  geo.wa=rbind(geo.wa,cq(WA,'04000US53',state)); # add entry for WA state
+  ## get 'other' entries
+  places.other=param(places.other);
+  geo.other=merge(geo,places.other);             # join on state, county
+  geo.other=geo.other[,cq(state,geoid,place)];   # rename and reorder columns to match WA
+  geo=rbind(geo.wa,geo.other)
+  save_geo(geo,base=base);
+}
+## stateid - just convert colnames to lower case, store in meta directory
+import_stateid=function(infile=param(stateid.infile),base=param(stateid.file)) {
+  if (param(verbose)) print(paste('>>> importing stateid',infile));
+  stateid=read.delim(infile,stringsAsFactors=FALSE);
+  colnames(stateid)=tolower(colnames(stateid));
+  save_stateid(stateid,base=base);
+}
 
-## ---- Save and Load population metadata ----
-## base includes path. suffix optional
-save_popbyage=function(pop,base=param(popbyage),suffix=cq(txt,RData)) {
-  param(save.meta,save.txt.meta);
-  save_(pop,base=base,save=save.meta,save.txt=FALSE,suffix=suffix);
-  if (save.txt.meta) {
-    ## want explicit age column in text file
-    pop=data.frame(age=rownames(pop),pop);
-    file=resuffix(base,old.suffix=suffix,suffix='txt');
-    write.table(pop,file=file,sep='\t',quote=F,row.names=F);
-  }
-}
-load_popbyage=function(base=param(popbyage)) {
-  pop=load_(base=base);
-  param(pop=pop);
-}
-read_popbyage=function(base=param(popbyage)) {
-  pop=read_(base=base,row.names='age');
-  param(pop=pop);
-}
-## read_pop=function(places=NULL,ages=NULL,file=filename(param(metadir),'population.txt')) {
-##   pop=read.delim(file,row.names='age',stringsAsFactors=FALSE);
-##   pop=filter_pop(pop,places,ages);      # nop if places, ages NULL
-##   pop;
-## }
-## ---- Filter and format population metadata ----
+## ---- Filter, compare, format population metadata ----
 filter_pop=function(pop,places=NULL,ages=NULL) {
   if (!is.null(places)) pop=pop[,places,drop=FALSE];
   if (!is.null(ages)) pop=pop[ages,,drop=FALSE];
   pop;
-}
-places_all=function(pop=param(pop)) {
-  if (is.null(pop)) pop=load_popbyage();
-  colnames(pop);
-}
-places_wa=function(pop=param(pop)) {
-  places.all=places_all(pop);
-  places.away=grep('_',places.all,value=T);
-  places.all %-% places.away;
-}
-ages_all=function(pop=param(pop)) {
-  if (is.null(pop)) pop=load_popbyage();
-  rownames(pop);
 }
 ## compare pops from multiple, possibly edited, objects
 ## returns whether pops all equal
@@ -181,7 +139,7 @@ cmp_pops=function(objs,places=NULL,ages=NULL,pop=param(pop),
   if (all(orig)) return(TRUE);          # all objects 'original', so perforce equal
   ## some objects edited. have to do merge & compare these vs original pop and each other
   pops=pops[!orig];
-  if (any(orig)) pops=c(list(load_popbyage()),pops); # include original pop in list to be merged
+  if (any(orig)) pops=c(list(load_pop()),pops); # include original pop in list to be merged
   ## tack 'age' onto each pop, filter, and order by age (for testing equality later)
   pops=lapply(pops,function(pop) {
     pop=filter_pop(pop,places,ages);
@@ -197,7 +155,6 @@ cmp_pops=function(objs,places=NULL,ages=NULL,pop=param(pop),
          "Sorry I can't be more specific");
   ok;
 }
-
 ## convert data frame of absolute counts into per capita counts
 ## express as counts per million. TODO: paramerize scale factor
 ## works for multiple age groups for one place or one age group for multiple places
@@ -208,5 +165,31 @@ per_capita=function(data,pop,places='state',ages='all') {
   if (length(ages)>1) pop=data.frame(t(pop[,places,drop=F]),check.names=FALSE);    
   pop=repr(pop,nrow(data));
   data.frame(date=data$date,round(1e6*data[,-1]/pop),check.names=FALSE);
+}
+
+## ---- Access metadata ----
+places_all=function(geo=param(geo)) {
+  if (is.null(geo)) geo=load_geo();
+  geo$place;
+}
+places_wa=function(geo=param(geo)) {
+  if (is.null(geo)) geo=load_geo();
+  geo$place[geo$state=='WA'];
+}
+places_other=function(geo=param(geo)) {
+  if (is.null(geo)) geo=load_geo();
+  geo$place[geo$state!='WA'];
+}
+ages_all=function(pop=param(pop)) {
+  if (is.null(pop)) pop=load_pop();
+  rownames(pop);
+}
+state_name2id=function(name,stateid=param(stateid)) {
+  if (is.null(stateid)) stateid=load_stateid();
+  sapply(name,function(name) stateid$id[stateid$name==name]);
+}
+state_id2name=function(id,stateid=param(stateid)) {
+  if (is.null(stateid)) stateid=load_stateid();
+  sapply(id,function(id) stateid$name[stateid$id==id]);
 }
 
