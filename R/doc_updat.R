78ec5c42d2804ecd255701c73124fe3a43d97c45
natgoodman <natg@shore.net>
2021-02-17 14:01:54 -0800

    Changes for version 21-02-14. Added back code in 'make_updat_objs' removed in previous version; obsolete now but still needed for old versions

--- R/doc_updat.R
+++ R/doc_updat.R
@@ -17,11 +17,11 @@
 ## no sections.
 doc_updat=function(need.objs=TRUE,need.init=TRUE,version='latest',transforms=NULL,...) {
   if (is.null(version)||version=='latest') version=max(sapply(cq(jhu,doh),latest_version));
-  if (is.null(transforms))
-    transforms=if (version<'20-12-20') list(jhu=roll,doh=c(roll,extra))
-               else if (version=='20-12-20') list(jhu=roll)
-               else list(jhu=fit_updat_objs,doh=c(extra,fit_updat_objs));                      
   if (need.objs) {
+    if (is.null(transforms))
+      transforms=if (version<'20-12-20') list(jhu=roll,doh=c(roll,extra))
+                 else if (version=='20-12-20') list(jhu=roll)
+                 else if (version<'21-02-07') list(jhu=fit_updat_objs,doh=c(extra,fit_updat_objs));
     make_updat_objs(version=version,transforms=transforms);
   }
   if (need.init) init_doc(doc='updat',version=version,...);
@@ -89,28 +89,46 @@ doc_updat=function(need.objs=TRUE,need.init=TRUE,version='latest',transforms=NUL
   }
   ## version 20-12-20: DOH update not available. hopefully temporary...
   if (version=='20-12-20') return();
-  ## Figures 3a-d cases by age
-  figblk_start();
-  ages=ages_all()[-1];
-  ages=c('0_19','20_39','40_59','60_79','80_');
-  ##  col=rev(col_brew(6,'viridis'))[-1];
-  col=col_brew(5,'d3');
-  sapply(cq(state,King,Snohomish,Pierce),function(place) 
+  ## starting 21-02-14. I only show statewise by-age results. other place similar
+  if (version<'21-02-14') {
+    ## Figures 3a-d cases by age
+    figblk_start();
+    ages=c('0_19','20_39','40_59','60_79','80_');
+    ##  col=rev(col_brew(6,'viridis'))[-1];
+    col=col_brew(5,'d3');
+    sapply(cq(state,King,Snohomish,Pierce),function(place) 
+      dofig(paste(sep='_','cases',place),
+            plot_cvdat(
+              doh.cases,places=place,ages=ages,per.capita=TRUE,lwd=2,col=col,
+              title=figtitle(paste("Weekly cases per million by age in",labels.wa[place])))));
+    ## Figures 4a-d deaths
+    figblk_start();
+    ages=c('0_59','60_79','80_');
+    col=col[c(1,4,5)];
+    sapply(cq(state,King,Snohomish,Pierce),function(place) 
+      dofig(paste(sep='_','deaths',place),
+            plot_cvdat(
+              doh.deaths,places=place,ages=ages,per.capita=TRUE,lwd=2,col=col,
+              title=figtitle(paste("Weekly deaths per million by age in",labels.wa[place])),
+              legend='top')));
+  } else {
+    figblk_end();
+    ##  col=rev(col_brew(6,'viridis'))[-1];
+    col=col_brew(5,'d3');
+    place='state';
+    ## Figure 3 statewide cases by age
+    ages=c('0_19','20_39','40_59','60_79','80_');
     dofig(paste(sep='_','cases',place),
           plot_cvdat(
             doh.cases,places=place,ages=ages,per.capita=TRUE,lwd=2,col=col,
-            title=figtitle(paste("Weekly cases per million by age in",labels.wa[place])))));
-  ## Figures 4a-d deaths
-  figblk_start();
-  ages=c('0_59','60_79','80_');
-  col=col[c(1,4,5)];
-  sapply(cq(state,King,Snohomish,Pierce),function(place) 
+            title=figtitle(paste("Weekly cases per million by age in",labels.wa[place]))));
+    ## Figure 4 statewide deaths by age
+    ages=c('0_59','60_79','80_');
     dofig(paste(sep='_','deaths',place),
-        plot_cvdat(
-          doh.deaths,places=place,ages=ages,per.capita=TRUE,lwd=2,col=col,
-          title=figtitle(paste("Weekly deaths per million by age in",labels.wa[place])),
-          legend='top')));
-
+          plot_cvdat(
+            doh.deaths,places=place,ages=ages,per.capita=TRUE,lwd=2,col=col,
+            title=figtitle(paste("Weekly deaths per million by age in",labels.wa[place]))));
+  }
   ## Figures 5a-b compare DOH, JHU.
   ## Note: not used in versions after Dec 13. Will probably come back...
   figblk_start();
@@ -140,24 +158,35 @@ doc_updat=function(need.objs=TRUE,need.init=TRUE,version='latest',transforms=NUL
             places=cq(state),ages='all',per.capita=TRUE,lwd=2,
             title=figtitle("Weekly deaths per million: DOH and JHU (Washington state)"),
             legends=list(title='Data Source',labels=c('DOH','JHU'))));
-  }
-  else {
+  } else {
     dofig('cases_dohjhu',plot_dohjhu('cases'));
     dofig('deaths_dohjhu',plot_dohjhu('deaths'));
   }
-  ## Tables 1-2 trend analysis. not used in document.
+  ## Tables 1-2 trend analysis. Tables 3-4 raw data counts. not used in document.
   if (version>='21-02-07') {
-    if (param(verbose)) print(paste('+++ making figures'));
+    if (param(verbose)) print(paste('+++ making tables'));
     trend.cases=trend(jhu.cases.raw,places=c(places.wa,places.nonwa));
     trend.deaths=trend(jhu.deaths.raw,places=c(places.wa,places.nonwa));
+    counts.cases=data_cvdat(jhu.cases.raw,places=c(places.wa,places.nonwa),per.capita=TRUE);
+    counts.deaths=data_cvdat(jhu.deaths.raw,places=c(places.wa,places.nonwa),per.capita=TRUE);
     ## TODO: rewrite using dotbl when implemented!
     ## dotbl('trend_cases',emit_trend,data=trend.cases,title='Trend analysis for cases');
     ## dotbl('trend_deaths',emit_trend,data=trend.deaths,title='Trend analysis for deaths');
-    tbldir=param(tbldir);
+    param(tbldir,pjto);
+    ## Tables 1-2 trend analysis
     file=file.path(tbldir,'table_001_trend_cases.txt');
     write.table(trend.cases,file=file,sep='\t',quote=F,row.names=F);
+    if (pjto) system(paste('pjto',file));           # copy to Mac if desired (usally is)
     file=file.path(tbldir,'table_002_trend_deaths.txt');
     write.table(trend.deaths,file=file,sep='\t',quote=F,row.names=F);
+    if (pjto) system(paste('pjto',file));           # copy to Mac if desired (usally is)
+    ## Tables 3-4 raw data counts
+    file=file.path(tbldir,'table_003_counts_cases.txt');
+    write.table(counts.cases,file=file,sep='\t',quote=F,row.names=F);
+    if (pjto) system(paste('pjto',file));           # copy to Mac if desired (usally is)
+    file=file.path(tbldir,'table_003_counts_deaths.txt');
+    write.table(counts.deaths,file=file,sep='\t',quote=F,row.names=F);
+    if (pjto) system(paste('pjto',file));           # copy to Mac if desired (usally is)
   }
   invisible();
 }
@@ -206,68 +235,94 @@ plot_dohjhu=function(what=cq(cases,deaths)) {
     title=title,type='p',add=TRUE,pch=c(20,20));
 }
 
-## generate trend tables
-## each table yields two files: one with complete data, other formated for Rmd
-trend_table=function(tbl,title) {
-
-
-}
-
 ## make objs doc_updat needs
+## NG 21-02-17: oops. still need transform pipeline for old versions
+##   calling function (doc_updat) sets trnsforms if needed
 ## NG 21-02-09: abandoned the cute transform pipeline
 ##   too many exceptions and with only two sources, not worth the trouble
 ## NG 20-12-14: fixed longstanding bug. have to do 'extra' before 'editing' object to create
 ##   new places or ages, else objects will be incompatible.
 ##   bug in 'extra' caused error to be missed and results of edited places and ages to be 0!
-make_updat_objs=function(what=cq(cases,deaths),datasrc=cq(doh,jhu),version='latest') {
-  cases=expand.grid(what=what,datasrc=datasrc,stringsAsFactors=FALSE);
-  withrows(cases,case,{
-    if (param(verbose)) print(paste('+++ making',datasrc,what));
-    ## start with raw. really 'weekly, incremental'
-    obj=raw(what,datasrc,version);    # start with raw
-    assign(paste(sep='.',datasrc,what,'raw'),obj,globalenv());  # save as 'raw'
-    obj=switch(datasrc,               # transform as needed for src
-               doh=doh_updat_objs(obj,what),
-               jhu=jhu_updat_objs(obj,what));
-    assign(paste(sep='.',datasrc,what),obj,globalenv())        # save as 'final'
-  });
-  cases;
-}
-doh_updat_objs=function(obj,what) {
-  obj=edit(obj,KEEP=cq(state,King,Snohomish,Pierce));
-  version=version(obj);
-  if (version%in%c('21-01-24','21-01-31')) {
-    ## versions 21-01-24, 21-01-31 have problems... not exactly the same natch...
-    ## in 21-01-24, data before 2020-01-26 is anomalously high, eg, 2020-01-12 has 8779 cases!
-    ## in 21-01-31, data before 2020-02-02 is anomalously high, eg, 2020-01-12 has 10331 cases!
-    ## both versions missing data after 21-01-10!
-    ##   21-01-24, should have week of 21-01-17 but ends at 21-01-10
-    ##   21-01-31, should have week of 21-01-24 but ends at 21-01-10
-    min.date=if(version=='21-01-24') '2020-01-26' else '2020-02-02'
-    obj=edit(obj,date>=min.date);
-    assign(paste(sep='.','doh',what,'raw'),obj,globalenv());
-    ## must do 'extra' before editing out last weeks, else code crashes
-    obj=extra(obj); 
-    obj=edit(obj,date<='2021-01-10');   # edit out missing weeks
-    assign(paste(sep='.','doh',what,'extra'),obj,globalenv());
-  } else {
-    assign(paste(sep='.','doh',what,'raw'),obj,globalenv()); 
-    obj=extra(obj);
-    assign(paste(sep='.','doh',what,'extra'),obj,globalenv());
+make_updat_objs=
+  function(what=cq(cases,deaths),datasrc=cq(doh,jhu),version='latest',transforms=NULL) {
+    cases=expand.grid(what=what,datasrc=datasrc,stringsAsFactors=FALSE);
+    if (is.null(transforms)) {
+      withrows(cases,case,{
+        if (param(verbose)) print(paste('+++ making',datasrc,what));
+        ## start with raw. really 'weekly, incremental'
+        obj=raw(what,datasrc,version);    # start with raw
+        obj=switch(datasrc,               # transform as needed for src
+                   doh=doh_updat_objs(obj,what),
+                   jhu=jhu_updat_objs(obj,what));
+        assign(paste(sep='.',datasrc,what),obj,globalenv())        # save as 'final'
+      });
+    } else {
+      ## R needs each transform entry to be a list for sapply below to work. sigh...
+      transforms=lapply(transforms,function(t) if (length(t)==1) list(t) else t);
+      withrows(cases,case,{
+        if (param(verbose)) print(paste('+++ making',datasrc,what));
+        ## start with raw. really 'weekly, incremental'
+        obj=raw(what,datasrc,version);    # start with raw
+        obj=switch(datasrc,               # transform as needed for src
+                   doh=edit(obj,KEEP=cq(state,King,Snohomish,Pierce)),
+                   jhu=weekly(incremental(obj)),
+                   nyt=weekly(incremental(obj)),
+                   trk=weekly(obj));
+        assign(paste(sep='.',datasrc,what,'raw'),obj,globalenv());  # save as 'raw'
+        ## now work down transforms
+        if (datasrc=='doh'&&version%in%c('21-01-24','21-01-31')) {
+          ## doh 21-01-24, 21-01-31 have problems... not exactly the same natch...
+          ## do transforms explicitly
+          ## in 21-01-24, data before 2020-01-26 is too high, eg, 2020-01-12 has 8779 cases!
+          ## in 21-01-31, data before 2020-02-02 is too high, eg, 2020-01-12 has 10331 cases!
+          min.date=if(version=='21-01-24') '2020-01-26' else '2020-02-02'
+          obj=edit(obj,date>=min.date);
+          assign(paste(sep='.',datasrc,what,'raw'),obj,globalenv());  # save as 'raw'
+          ## must do 'extra' before editing out last weeks, else code crashes
+          obj=extra(obj); 
+          ## both versions missing data after 21-01-10!
+          ##   21-01-24, should have week of 21-01-17 but ends at 21-01-10
+          ##   21-01-31, should have week of 21-01-24 but ends at 21-01-10
+          obj=edit(obj,date<='2021-01-10');
+          ## edit out last weeks before doing fit, else 0s at end pull fit down
+          obj=fit_updat_objs(obj);
+        } else 
+          sapply(transforms[[datasrc]],function(f) obj<<-f(obj));
+        ## do final edit for doh
+        if (datasrc=='doh') obj=edit(obj,'0_59'='0_19'+'20_39'+'40_59');
+        assign(paste(sep='.',datasrc,what),obj,globalenv());        # save as 'final'
+      });
+    }
+    cases;
   }
+## hard-coded 'updat_objs' functions only used for 21-02-07 and later
+## controlled in doc_updat above by setting transforms or leaving it NULL
+doh_updat_objs=function(obj,what) {
+  version=version(obj);
+  if (version<'21-02-07')
+    stop("Bad news: 'doh_updat_objs' only handles versions 21-02-07 and later, not ",
+         version,". Should have been caught sooner");
+  obj=edit(obj,KEEP=cq(state,King,Snohomish,Pierce));
+  assign(paste(sep='.','doh',what,'raw'),obj,globalenv()); 
+  obj=extra(obj);
+  assign(paste(sep='.','doh',what,'extra'),obj,globalenv());
   obj=fit_updat_objs(obj,what);
   if (what=='deaths') obj=edit(obj,'0_59'='0_19'+'20_39'+'40_59');
   assign(paste(sep='.','doh',what),obj,globalenv());        # save as 'final'
   obj;
 }
 jhu_updat_objs=function(obj,what) {
+  version=version(obj);
+  if (version<'21-02-07')
+    stop("Bad news: 'jhu_updat_objs' only handles versions 21-02-07 and later, not ",
+         version,". Should have been caught sooner");
   obj=weekly(incremental(obj));
   assign(paste(sep='.','jhu',what,'raw'),obj,globalenv());
   obj=fit_updat_objs(obj,what);
   assign(paste(sep='.','jhu',what),obj,globalenv());        # save as 'final'
   obj;
 }
-  
+
 fit_updat_objs=function(obj,what) {
   ## use 1 day for cases, 10.5 days (1.5 weeks) for deaths
   fit.unit=if(what=='cases') 1 else 10.5;
